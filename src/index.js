0cdcee5ffd32f7fdcb52813c634f0eb48fa1f227
tushar_android <tushar.pandey@bold.com>
2021-01-03 10:25:25 +0530

    js bundle to react app cross communication added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: ''
+    }
+  }
+
+  setUserName(name) {
+    this.setState({ userName: name })
+  }
+
+  onClickEvent = () => {
+    window.ReactNativeWebView.postMessage(JSON.stringify({ userName: this.state.userName }));
+  }
+
+  myChangeHandler = (event) => {
+    this.setState({ userName: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{`i am ${this.state.userName}`}</h1>
+        <input
+          type='text'
+          onChange={this.myChangeHandler}
+        />
+        <button
+          className="btn btn-default"
+          style={{ padding: 10, margin: 10 }}
+          onClick={this.onClickEvent}>{"Get User Name Back"}</button>
+      </div >
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App ref={(appComponent) => { window.appComponent = appComponent }} />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
